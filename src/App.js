253decb16c8731bcc8a37ca8a21317145d1f99b0
lachgar <lachgar.m@gmail.com>
2024-10-27 00:08:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ const VideoGallery = () => {
                 style={{ cursor: 'pointer' }}
                 onClick={() => handleVideoClick(project.videoUrl)}
               >
-                <video width="100%" height="180" controls={false}>
+                <video width="100%" height="150" controls={false}>
                   <source src={project.videoUrl} type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
