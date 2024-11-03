c9abecdfd0cbe0c8dab2c1e03392725f27f3f5ef
lachgar <lachgar.m@gmail.com>
2024-11-03 02:33:43 +0100

    vidos

--- src/App.js
+++ src/App.js
@@ -6,44 +6,267 @@ import React, { useState } from 'react';
 import { Modal } from 'react-bootstrap';
 
 const projects = [
-  { number: 1, title: 'Interface graphique', developer: 'Nom 1', videoUrl: '/videos/video1.mp4', githubUrl: 'https://github.com/user/projet1' },
-  { number: 2, title: 'Pizza', developer: 'Nom 2', videoUrl: '/videos/video2.mp4', githubUrl: 'https://github.com/user/projet2' },
-  { number: 3, title: 'TabHost', developer: 'Nom 3', videoUrl: '/videos/video3.mp4', githubUrl: 'https://github.com/user/projet3' },
-  { number: 4, title: 'RecyclerView', developer: 'Nom 4', videoUrl: '/videos/video4.mp4', githubUrl: 'https://github.com/user/projet4' },
-  { number: 5, title: 'Volley + Image', developer: 'Nom 5', videoUrl: '/videos/video5.mp4', githubUrl: 'https://github.com/user/projet5' },
-  { number: 6, title: 'Localisation', developer: 'Nom 6', videoUrl: '/videos/video6.mp4', githubUrl: 'https://github.com/user/projet6' },
-  { number: 7, title: 'Map + Volley', developer: 'Nom 7', videoUrl: '/videos/video7.mp4', githubUrl: 'https://github.com/user/projet7' },
-  { number: 8, title: 'SQLite', developer: 'Nom 8', videoUrl: '/videos/video8.mp4', githubUrl: 'https://github.com/user/projet8' },
-  { number: 9, title: 'Fragment', developer: 'Nom 9', videoUrl: '/videos/video9.mp4', githubUrl: 'https://github.com/user/projet9' },
-  { number: 10, title: 'Navigation Drawer', developer: 'Nom 10', videoUrl: '/videos/video10.mp4', githubUrl: 'https://github.com/user/projet10' },
-  { number: 11, title: 'Notifications', developer: 'Nom 11', videoUrl: '/videos/video11.mp4', githubUrl: 'https://github.com/user/projet11' },
-  { number: 12, title: 'Capteurs', developer: 'Nom 12', videoUrl: '/videos/video12.mp4', githubUrl: 'https://github.com/user/projet12' },
-  { number: 13, title: 'Retrofit', developer: 'Nom 13', videoUrl: '/videos/video13.mp4', githubUrl: 'https://github.com/user/projet13' },
-  { number: 14, title: 'Room', developer: 'Nom 14', videoUrl: '/videos/video14.mp4', githubUrl: 'https://github.com/user/projet14' },
-  { number: 15, title: 'TabLayout + ViewPager/Fragment', developer: 'Nom 15', videoUrl: '/videos/video15.mp4', githubUrl: 'https://github.com/user/projet15' },
-  { number: 16, title: 'LiveData + ViewModel', developer: 'Nom 16', videoUrl: '/videos/video16.mp4', githubUrl: 'https://github.com/user/projet16' },
-  { number: 17, title: 'Data Binding', developer: 'Nom 17', videoUrl: '/videos/video17.mp4', githubUrl: 'https://github.com/user/projet17' },
-  { number: 18, title: 'Firebase', developer: 'Nom 18', videoUrl: '/videos/video18.mp4', githubUrl: 'https://github.com/user/projet18' },
-  { number: 19, title: 'Coroutines', developer: 'Nom 19', videoUrl: '/videos/video19.mp4', githubUrl: 'https://github.com/user/projet19' },
-  { number: 20, title: 'WorkManager', developer: 'Nom 20', videoUrl: '/videos/video20.mp4', githubUrl: 'https://github.com/user/projet20' },
-  { number: 21, title: 'Bluetooth', developer: 'Nom 21', videoUrl: '/videos/video21.mp4', githubUrl: 'https://github.com/user/projet21' },
-  { number: 22, title: 'Camera API', developer: 'Nom 22', videoUrl: '/videos/video22.mp4', githubUrl: 'https://github.com/user/projet22' },
-  { number: 23, title: 'ExoPlayer', developer: 'Nom 23', videoUrl: '/videos/video23.mp4', githubUrl: 'https://github.com/user/projet23' },
-  { number: 24, title: 'Firebase Cloud Messaging', developer: 'Nom 24', videoUrl: '/videos/video24.mp4', githubUrl: 'https://github.com/user/projet24' },
-  { number: 25, title: 'Jetpack Compose', developer: 'Nom 25', videoUrl: '/videos/video25.mp4', githubUrl: 'https://github.com/user/projet25' },
-  { number: 26, title: 'Permissions Runtime', developer: 'Nom 26', videoUrl: '/videos/video26.mp4', githubUrl: 'https://github.com/user/projet26' },
-  { number: 27, title: 'Gesture Detection', developer: 'Nom 27', videoUrl: '/videos/video27.mp4', githubUrl: 'https://github.com/user/projet27' },
-  { number: 28, title: 'Lottie Animations', developer: 'Nom 28', videoUrl: '/videos/video28.mp4', githubUrl: 'https://github.com/user/projet28' },
-  { number: 29, title: 'NFC', developer: 'Nom 29', videoUrl: '/videos/video29.mp4', githubUrl: 'https://github.com/user/projet29' },
-  { number: 30, title: 'Multithreading', developer: 'Nom 30', videoUrl: '/videos/video30.mp4', githubUrl: 'https://github.com/user/projet30' },
-  { number: 31, title: 'ConstraintLayout Avancé', developer: 'Nom 31', videoUrl: '/videos/video31.mp4', githubUrl: 'https://github.com/user/projet31' },
-  { number: 32, title: 'Sauvegarde d\'États', developer: 'Nom 32', videoUrl: '/videos/video32.mp4', githubUrl: 'https://github.com/user/projet32' },
-  { number: 33, title: 'Navigation Component', developer: 'Nom 33', videoUrl: '/videos/video33.mp4', githubUrl: 'https://github.com/user/projet33' },
-  { number: 34, title: 'Paging Library', developer: 'Nom 34', videoUrl: '/videos/video34.mp4', githubUrl: 'https://github.com/user/projet34' },
-  { number: 35, title: 'Tests Unitaires avec JUnit', developer: 'Nom 35', videoUrl: '/videos/video35.mp4', githubUrl: 'https://github.com/user/projet35' },
-  { number: 36, title: 'Tests Instrumentés avec Espresso', developer: 'Nom 36', videoUrl: '/videos/video36.mp4', githubUrl: 'https://github.com/user/projet36' },
-  { number: 37, title: 'Services Android', developer: 'Nom 37', videoUrl: '/videos/video37.mp4', githubUrl: 'https://github.com/user/projet37' },
-];
+  {
+      "Number": 1,
+      "Title": "Interface graphique",
+      "Developer": "Ait Ouahda Younes",
+      "Video URL": "/videos/video1.mp4",
+      "GitHub URL": "https://github.com/YounesAO/Android-kotlin-project-TP1"
+  },
+  {
+      "Number": 2,
+      "Title": "Pizza",
+      "Developer": "Amerga Younes",
+      "Video URL": "/videos/video2.mp4",
+      "GitHub URL": "https://github.com/Younessamg/pizza_mobile.git"
+  },
+  {
+      "Number": 3,
+      "Title": "TabHost",
+      "Developer": "Anejjar Ihssane",
+      "Video URL": "/videos/video3.mp4",
+      "GitHub URL": "https://github.com/Anejjar24/Gestion-taches"
+  },
+  {
+      "Number": 4,
+      "Title": "RecyclerView",
+      "Developer": "AZIZ Mohammed",
+      "Video URL": "/videos/video4.mp4",
+      "GitHub URL": "https://github.com/aziz-laravel/Projet4"
+  },
+  {
+      "Number": 5,
+      "Title": "Volley + Image",
+      "Developer": "Ballouk Mohamed",
+      "Video URL": "/videos/video5.mp4",
+      "GitHub URL": "https://github.com/Ballouk12/projet5.git"
+  },
+  {
+      "Number": 6,
+      "Title": "Localisation",
+      "Developer": "BEESAM Adam",
+      "Video URL": "/videos/video6.mp4",
+      "GitHub URL": "https://github.com/AdamBessam/Controle-Mobile"
+  },
+  {
+      "Number": 7,
+      "Title": "Map + Volley",
+      "Developer": "BOKTAYA Amine",
+      "Video URL": "/videos/video7.mp4",
+      "GitHub URL": "https://github.com/BoktayaAmine/dev_mobile_projects/tree/master/Google_Maps_App"
+  },
+  {
+      "Number": 8,
+      "Title": "SQLite",
+      "Developer": "BOUJNAH AsSia",
+      "Video URL": "/videos/video8.mp4",
+      "GitHub URL": "https://github.com/assia1905/TPSQLite"
+  },
+  {
+      "Number": 9,
+      "Title": "Fragment",
+      "Developer": "BOUKHRAIS Meryem",
+      "Video URL": "/videos/video9.mp4",
+      "GitHub URL": "https://github.com/Bou-Mery/MyNewsApp"
+  },
+  {
+      "Number": 10,
+      "Title": "Navigation Drawer",
+      "Developer": "Bouleknadel Abderrahmane",
+      "Video URL": "/videos/video10.mp4",
+      "GitHub URL": "https://github.com/bouleknadel/LibraryApp.git"
+  },
+  {
+      "Number": 11,
+      "Title": "Notifications",
+      "Developer": "BOUSENSAR Rajaa",
+      "Video URL": "/videos/video11.mp4",
+      "GitHub URL": "https://github.com/rajaa52/depot1.git"
+  },
+  {
+      "Number": 12,
+      "Title": "Capteurs",
+      "Developer": "CHAJARI Salma",
+      "Video URL": "/videos/video12.mp4",
+      "GitHub URL": "https://github.com/Salma-CHAJARI/SensorsProject.git"
+  },
+  {
+      "Number": 13,
+      "Title": "Retrofit",
+      "Developer": "CHOUAY Walid",
+      "Video URL": "/videos/video13.mp4",
+      "GitHub URL": "https://github.com/CHOUAY15/EnsaHealth"
+  },
+  {
+      "Number": 14,
+      "Title": "Room",
+      "Developer": "DAOUDI Mohammed",
+      "Video URL": "/videos/video14.mp4",
+      "GitHub URL": "https://github.com/Mohammedaoudi/ENSAJ-APP-Management"
+  },
+  {
+      "Number": 15,
+      "Title": "TabLayout + ViewPager/Fragment",
+      "Developer": "DOUIDY Sifeddine",
+      "Video URL": "/videos/video15.mp4",
+      "GitHub URL": "https://github.com/SaifeddineDouidy/Beginner-Projects-Mobile-JAVA/tree/master/Projet_Controle"
+  },
+  {
+      "Number": 16,
+      "Title": "LiveData + ViewModel",
+      "Developer": "DRIHAM Siham",
+      "Video URL": "/videos/video16.mp4",
+      "GitHub URL": "https://github.com/SihamDriham/TodoList"
+  },
+  {
+      "Number": 17,
+      "Title": "Data Binding",
+      "Developer": "EL ABIDI Aya",
+      "Video URL": "/videos/video17.mp4",
+      "GitHub URL": "https://github.com/yaelaya/TP-Visit_Paris.git"
+  },
+  {
+      "Number": 18,
+      "Title": "Firebase",
+      "Developer": "EL GHARBI ABDELLAH",
+      "Video URL": "/videos/video18.mp4",
+      "GitHub URL": "https://github.com/abdellah-elgharbi/chat.git"
+  },
+  {
+      "Number": 19,
+      "Title": "Coroutines",
+      "Developer": "EL MAHJOUBI Soukaina",
+      "Video URL": "/videos/video19.mp4",
+      "GitHub URL": "elmahjoubisouka/Mini_projet_application_Quiz"
+  },
+  {
+      "Number": 20,
+      "Title": "WorkManager",
+      "Developer": "EL MASKYNE Mohamed Amine",
+      "Video URL": "/videos/video20.mp4",
+      "GitHub URL": "https://github.com/MOHAMED-AMINE11/E-MEDEC.git"
+  },
+  {
+      "Number": 21,
+      "Title": "Bluetooth",
+      "Developer": "ELBAHLOULI Salma",
+      "Video URL": "/videos/video21.mp4",
+      "GitHub URL": "https://github.com/salma510/Bluetooth"
+  },
+  {
+      "Number": 22,
+      "Title": "Camera API",
+      "Developer": "ELHALLA Zineb",
+      "Video URL": "/videos/video22.mp4",
+      "GitHub URL": "https://github.com/ZinebELHALLA/CamApp"
+  },
+  {
+      "Number": 23,
+      "Title": "ExoPlayer",
+      "Developer": "ELLOUBAB Aya",
+      "Video URL": "/videos/video23.mp4",
+      "GitHub URL": "https://github.com/aya-elloubab/ExoPlayer-Android-Application"
+  },
+  {
+      "Number": 24,
+      "Title": "Firebase Cloud Messaging",
+      "Developer": "Fattouhi Radwa",
+      "Video URL": "/videos/video24.mp4",
+      "GitHub URL": "https://github.com/Radwa-f/FCM_Project.git"
+  },
+  {
+      "Number": 25,
+      "Title": "Jetpack Compose",
+      "Developer": "Fihri Yasmine",
+      "Video URL": "/videos/video25.mp4",
+      "GitHub URL": "https://github.com/yasminefhr1/MovieApp_Contole"
+  },
+  {
+      "Number": 26,
+      "Title": "Permissions Runtime",
+      "Developer": "HARATI Ayoub",
+      "Video URL": "/videos/video26.mp4",
+      "GitHub URL": "https://github.com/ayoub-aav/TPs-CoursMobile/tree/main/permission"
+  },
+  {
+      "Number": 27,
+      "Title": "Gesture Detection",
+      "Developer": "Khalil Fatima",
+      "Video URL": "/videos/video27.mp4",
+      "GitHub URL": "https://github.com/fatikhalil/projet1.git"
+  },
+  {
+      "Number": 28,
+      "Title": "Lottie Animations",
+      "Developer": "Lahlou Asmae",
+      "Video URL": "/videos/video28.mp4",
+      "GitHub URL": "https://github.com/lahlouasmae/FlowerShop"
+  },
+  {
+      "Number": 29,
+      "Title": "NFC",
+      "Developer": "Lahlyal Ahmed Moubarak",
+      "Video URL": "/videos/video29.mp4",
+      "GitHub URL": "https://github.com/amlmbr/GestionNFC"
+  },
+  {
+      "Number": 30,
+      "Title": "Multithreading",
+      "Developer": "MAGHNI Manal",
+      "Video URL": "/videos/video30.mp4",
+      "GitHub URL": "https://github.com/ManalMaghni/Projet/tree/master"
+  },
+  {
+      "Number": 31,
+      "Title": "ConstraintLayout Avancé",
+      "Developer": "Mandour Ilyass",
+      "Video URL": "/videos/video31.mp4",
+      "GitHub URL": "https://github.com/ilyassman/EnsajFood/"
+  },
+  {
+      "Number": 32,
+      "Title": "Sauvegarde d'États",
+      "Developer": "Miskar Amina",
+      "Video URL": "/videos/video32.mp4",
+      "GitHub URL": "https://github.com/miskaraminaa/NoteAPP_ViewModel.git"
+  },
+  {
+      "Number": 33,
+      "Title": "Navigation Component",
+      "Developer": "Sabrou hafsa",
+      "Video URL": "/videos/video33.mp4",
+      "GitHub URL": "https://github.com/Hafsa1sa/CupcakeCorner.git"
+  },
+  {
+      "Number": 34,
+      "Title": "Paging Library",
+      "Developer": "Sakhr niama",
+      "Video URL": "/videos/video34.mp4",
+      "GitHub URL": "https://github.com/niama22/Projet34-mobile.git"
+  },
+  {
+      "Number": 35,
+      "Title": "Tests Unitaires avec JUnit",
+      "Developer": "Taghti Zineb",
+      "Video URL": "/videos/video35.mp4",
+      "GitHub URL": "https://github.com/zinebtaghti/To_Do_List"
+  },
+  {
+      "Number": 36,
+      "Title": "Tests Instrumentés avec Espresso",
+      "Developer": "ZAHIR Oumaima",
+      "Video URL": "/videos/video36.mp4",
+      "GitHub URL": "https://github.com/ZahirOuma/MyLibraryApp/tree/master"
+  },
+  {
+      "Number": 37,
+      "Title": "Services Android",
+      "Developer": "ZENNOURI Nassima",
+      "Video URL": "/videos/video37.mp4",
+      "GitHub URL": "https://github.com/NassimaZENNOURI/ProjetMusique"
+  }
+]
+;
 
 const VideoGallery = () => {
   const [showModal, setShowModal] = useState(false);
