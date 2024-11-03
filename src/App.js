6065378b7302f49c40904b5b2e0a8d0869683234
lachgar <lachgar.m@gmail.com>
2024-11-03 02:46:11 +0100

    vidoes

--- src/App.js
+++ src/App.js
@@ -287,7 +287,7 @@ const VideoGallery = () => {
       
       <div className="container">
       <header className="text-center mb-5">
-        <h3 className="display-4">Contrôle pratique programmation mobile</h3>
+        <h3 className="display-4">Contrôle pratique programmation mobile (Kotlin)</h3>
         <h4 className="text-muted">2ITE ENSA EL JADIDA</h4>
         <h5 className="text-secondary">Pr. Mohamed LACHGAR</h5>
         <p style={{ color: "red" }}>
